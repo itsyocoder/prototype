4cc5acaa772fbb9856fb403304c6f08daa98ac33
PJ <PJ@PRISMX>
2024-11-18 08:21:25 +0800

    login updateAdd project files.

---
+++ View/verify.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace prototype.View
+{
+    /// <summary>
+    /// Interaction logic for verify.xaml
+    /// </summary>
+    public partial class verify : Window
+    {
+        public verify()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnmin_click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnexit_click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnverify_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
