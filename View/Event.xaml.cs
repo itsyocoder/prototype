b42467b6dcaff9f8fbc4f72edf50bb00eef38b53
bin-binelle <jeanelle.labsan1211@gmail.com>
2024-12-03 19:42:23 +0800

    Add project files.

---
+++ View/Event.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Windows.Controls;
+using Microsoft.Data.SqlClient;
+
+namespace prototype.View
+{
+    public partial class Event : UserControl
+    {
+        // Connection string for your SQL Server
+        private readonly string connectionString = @"Server=MSI\SQLEXPRESS01; Database=LoginDB; Integrated Security=True; Encrypt=True; TrustServerCertificate=True";
+
+        public Event()
+        {
+            InitializeComponent();
+            LoadEventData(); // Load the data when the UserControl initializes
+        }
+
+        private void LoadEventData()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT * FROM Events"; // Query to get all data from the Events table
+                    SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+
+                    // Bind the data to the DataGrid
+                    EventDataGrid.ItemsSource = dataTable.DefaultView;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Show error message if fetching fails
+                System.Windows.MessageBox.Show($"Error loading data: {ex.Message}", "Error", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+            }
+        }
+    }
+}
